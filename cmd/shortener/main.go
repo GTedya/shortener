f0edab3e1fa96830d6c921902abd0385f29bd3ec
Георгий Тедеев <georgi.tedeev@mail.ru>
2023-10-30 18:27:35 +0300

    Правка

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,58 +1,86 @@
 package main
 
 import (
+	"fmt"
 	"github.com/gorilla/mux"
 	"io"
 	"math/rand"
 	"net/http"
+	"net/url"
 )
 
-var m map[string]string
+var urlMap map[string]string
 
 func main() {
-	m = make(map[string]string)
+	urlMap = make(map[string]string)
+
 	router := mux.NewRouter()
 
-	router.HandleFunc("/", createURL).Methods(http.MethodPost)
-
 	router.HandleFunc("/{id}", getURLByID).Methods(http.MethodGet)
+	router.HandleFunc("/", createURL).Methods(http.MethodPost)
 
 	err := http.ListenAndServe(":8080", router)
 	if err != nil {
-		panic(err)
+		fmt.Println(err)
+		return
 	}
 }
 
 func createURL(w http.ResponseWriter, r *http.Request) {
 	if contentType := r.Header.Get("Content-Type"); contentType == "text/plain" {
-		url, err := io.ReadAll(r.Body)
+		body, err := io.ReadAll(r.Body)
+
 		if err != nil {
 			w.WriteHeader(http.StatusBadRequest)
 			return
 		}
+		if len(body) == 0 {
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
 
-		id := randStr(8)
-		m[id] = string(url)
-		w.Header().Set("Content-Type", "text/plain")
+		id := generateTestURL(8)
+		encodedID := url.PathEscape(id)
+		urlMap[encodedID] = string(body)
 
-		http.Redirect(w, r, "http://localhost:8080/"+id, http.StatusCreated)
+		w.Header().Add("Content-Type", "text/plain")
+		w.WriteHeader(http.StatusCreated)
+
+		_, err = w.Write([]byte("http://localhost:8080/" + encodedID))
+		if err != nil {
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+	} else {
+		w.WriteHeader(http.StatusBadRequest)
+		return
 	}
 }
 
 func getURLByID(w http.ResponseWriter, r *http.Request) {
-	id := mux.Vars(r)["id"]
-	url, ok := m[id]
-	if !ok {
+	if contentType := r.Header.Get("Content-Type"); contentType == "text/plain" {
+		vars := mux.Vars(r)
+		id, ok := vars["id"]
+		if !ok {
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+		link, ok := urlMap[id]
+		if !ok {
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+		w.Header().Add("Content-Type", "text/plain")
+		http.Redirect(w, r, link, http.StatusTemporaryRedirect)
+	} else {
 		w.WriteHeader(http.StatusBadRequest)
 		return
 	}
-
-	http.Redirect(w, r, url, http.StatusTemporaryRedirect)
 }
 
 var charset = []byte("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
 
-func randStr(n int) string {
+func generateTestURL(n int) string {
 	b := make([]byte, n)
 	for i := range b {
 		b[i] = charset[rand.Intn(len(charset))]
