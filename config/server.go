9403e500d7583581bb1e29d011571a9dd6b4e052
Георгий Тедеев <georgi.tedeev@mail.ru>
2023-11-04 21:17:35 +0300

    Правка

--- config/server.go
+++ config/server.go
@@ -2,6 +2,7 @@ package config
 
 import (
 	"flag"
+	"os"
 )
 
 var FlagRunAddr string
@@ -11,4 +12,12 @@ func ParseFlags() {
 	flag.StringVar(&FlagRunAddr, "a", "localhost:8080", "address and port to run server")
 	flag.StringVar(&BasicURL, "b", "", "basic shorten URL")
 	flag.Parse()
+
+	if os.Getenv("SERVER_ADDRESS") != "" {
+		FlagRunAddr = os.Getenv("SERVER_ADDRESS")
+	}
+
+	if os.Getenv("BASE_URL") != "" {
+		BasicURL = os.Getenv("BASE_URL")
+	}
 }
