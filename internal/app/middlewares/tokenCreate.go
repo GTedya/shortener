47073c75a4e23fb1a88e86de7d10ba647d1904a6
Георгий Тедеев <georgi.tedeev@mail.ru>
2024-04-21 10:24:57 +0300

    16

--- internal/app/middlewares/tokenCreate.go
+++ internal/app/middlewares/tokenCreate.go
@@ -2,7 +2,6 @@ package middlewares
 
 import (
 	"context"
-	"errors"
 	"net/http"
 	"time"
 
@@ -26,27 +25,21 @@ func (m Middleware) TokenCreate(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		var cooks *http.Cookie
 
-		cooks, err := r.Cookie(tokenCookie)
-		if !errors.Is(err, http.ErrNoCookie) && err != nil {
-			m.Log.Errorf("cookie receiving error: %w", err)
+		token := jwt.NewWithClaims(jwt.SigningMethodHS256, Claims{
+			RegisteredClaims: jwt.RegisteredClaims{
+				ExpiresAt: jwt.NewNumericDate(time.Now().Add(TokenExp)),
+			},
+		})
+
+		tokenString, err := token.SignedString([]byte(SecretKey))
+		if err != nil {
+			m.Log.Errorf("token signed error: %w", err)
 			return
 		}
-		if errors.Is(err, http.ErrNoCookie) {
-			token := jwt.NewWithClaims(jwt.SigningMethodHS256, Claims{
-				RegisteredClaims: jwt.RegisteredClaims{
-					ExpiresAt: jwt.NewNumericDate(time.Now().Add(TokenExp)),
-				},
-			})
+		m.Log.Debug("token created")
 
-			tokenString, err := token.SignedString([]byte(SecretKey))
-			if err != nil {
-				m.Log.Errorf("token signed error: %w", err)
-				return
-			}
-
-			cooks = &http.Cookie{Name: tokenCookie, Value: tokenString}
-			http.SetCookie(w, cooks)
-		}
+		cooks = &http.Cookie{Name: tokenCookie, Value: tokenString}
+		http.SetCookie(w, cooks)
 
 		ctx := context.WithValue(r.Context(), TokenContextKey, cooks.Value)
 		r = r.WithContext(ctx)
